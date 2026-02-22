e8221227ce90bd1a880adc004a209a1c408b2ab0
AbdulHannanMalik339 <abdulhannanmalik96@gmail.com>
2026-02-22 22:39:51 +0500

    Data Types
    da

---
+++ datatypes.py
@@ -0,0 +1,20 @@
+name = "Hannan"
+age = 13
+is_student = True
+wegiht = 40
+print("Name:", name)
+print("Data type of name:", type(name))
+print("Age:", age)
+print("Data type of age:", type(age))
+print("Is student:", is_student)
+print("Data type of is_student:", type(is_student))
+print("Weight:", wegiht)
+print("Data type of weight:", type(wegiht)) 
+
+print("/n After Type Casting.....")
+age = str(age)
+print(age)
+print("Data types of age is:", type(age))
+wegiht = int(wegiht)
+print(wegiht)
+print("Data type of weight is:", type(wegiht))
