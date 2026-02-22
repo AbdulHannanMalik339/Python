c8433922d2aea3e909eeafb6d82992bf51290811
AbdulHannanMalik339 <abdulhannanmalik96@gmail.com>
2026-02-22 23:17:58 +0500

    Add files via upload

---
+++ StringOperation.py
@@ -0,0 +1,15 @@
+first_name = "Hannan"
+last_name = "Malik"
+full_name = first_name + last_name
+example = "haa"*5
+
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Full Name:", full_name)
+print("String Muplied 5 times gives this reult:", example)
+
+word = "Python"
+print("Lenght of string :", len(word))
+print("Firts Lettter of string:", word[0])
+print("Last Letter of string:", word[5])
+print("String Sliced :", word[0:3])
