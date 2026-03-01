0504144bf2845d1b62e5f2dd4f526987cefb7870
AbdulHannanMalik339 <abdulhannanmalik96@gmail.com>
2026-03-01 22:47:21 +0500

    Add files via upload

---
+++ extra.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+for i in range(1):
+    print(name)
+    print(age)  
---
+++ extra2.py
@@ -0,0 +1,11 @@
+while True:
+    name = input("Enter a name: ")
+    age = int(input("Enter your age: "))
+    print(name)
+    print(age)
+
+    t = input("Enter your anwer")
+
+    if t == "yes":
+        print("Thanks for your answer")
+        break
