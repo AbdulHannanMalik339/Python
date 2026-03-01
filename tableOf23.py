efb9043d7bb54d8bab46dd00b21d02b4df609274
AbdulHannanMalik339 <abdulhannanmalik96@gmail.com>
2026-03-01 22:17:42 +0500

    Add files via upload

---
+++ tableOf23.py
@@ -0,0 +1,5 @@
+# Python programm to print the table of 23
+#Loop from 1 to 10
+for i in range(1, 11):
+    #Print the result of 23 multiplied by the current value of i
+    print(f"23 x {i} = {23 * i}")
