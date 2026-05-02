29827e519fb89e477faec2836ec7afd5d1f61258
AbdulHannanMalik339 <abdulhannanmalik96@gmail.com>
2026-05-02 22:28:09 +0500

    Add files via upload

---
+++ O(1)time.py
@@ -0,0 +1,10 @@
+def printnumber (n) :
+    iterations = 0
+    print ("The number entered is: ", n)
+    iterations += 1
+    print ("The total iterations are: ", iterations,"\n")
+
+printnumber (10)
+printnumber (20)
+print ("\n With any 'n' the iterations will always be 1, so the time complexity is O(1)")
+
